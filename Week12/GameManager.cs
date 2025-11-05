7c757e7d43748cf32e8ed209ef2b696fb936f35c
Summer Smith <summersmith2676@gmail.com>
2025-11-05 16:04:52 -0500

    Add week12 and separate folders

---
+++ Week12/GameManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+    public GameObject enemyOnePrefab;
+    public GameObject cloudPrefab;
+
+    public TextMeshProUGUI livesText;
+    public TextMeshProUGUI scoreText;
+
+    public float horizontalScreenSize;
+    public float verticalScreenSize;
+
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        horizontalScreenSize = 10f;
+        verticalScreenSize = 6.5f;
+        score = 0;
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        CreateSky();
+        InvokeRepeating("CreateEnemy", 1, 3);
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateEnemy()
+    {
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.9f, verticalScreenSize, 0), Quaternion.Euler(180, 0, 0));
+    }
+
+    void CreateSky()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloudPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize), Random.Range(-verticalScreenSize, verticalScreenSize), 0), Quaternion.identity);
+        }
+        
+    }
+    public void AddScore(int earnedScore)
+    {
+        score = score + earnedScore;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void ChangeLivesText (int currentLives)
+    {
+        livesText.text = "Lives: " + currentLives;
+    }
+}
---
+++ Week12/README.md
@@ -0,0 +1,9 @@
+# DIG3480
+Week 11 Assignment
+- Contributors:
+- Emma Lundquist
+- Summer Smith
+
+Task 1 - Completed by Emma Lundquist. Edited player.cs
+
+Task 2 - Completed by Summer Smith. Edited GameManager.cs and made EnemyTwo.cs
