fbba039903d340490551bf41b3fd422538f518b5
Summer Smith <summersmith2676@gmail.com>
2025-11-05 14:20:48 -0500

    Updated project

--- Fighter Planes Spring 2025/Assets/Scripts/Enemy.cs
+++ Fighter Planes Spring 2025/Assets/Scripts/Enemy.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    public GameObject explosionPrefab;
+    
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
-        if (transform.position.y < -6.5f)
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatDidIHit)
+    {
+        if(whatDidIHit.tag == "Player")
         {
+            whatDidIHit.GetComponent<PlayerController>().LoseALife();
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        } else if(whatDidIHit.tag == "Weapons")
+        {
+            Destroy(whatDidIHit.gameObject);
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            gameManager.AddScore(5);
             Destroy(this.gameObject);
         }
     }
--- Fighter Planes Spring 2025/Assets/Scripts/GameManager.cs
+++ Fighter Planes Spring 2025/Assets/Scripts/GameManager.cs
@@ -1,34 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
+    public GameObject playerPrefab;
     public GameObject enemyOnePrefab;
-    public GameObject enemyTwoPrefab;
+    public GameObject cloudPrefab;
+
+    public TextMeshProUGUI livesText;
+
+    public float horizontalScreenSize;
+    public float verticalScreenSize;
+
+    public int score;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("CreateEnemyOne", 1, 2);
-        InvokeRepeating("CreateEnemyTwo", 1.5f, 4);
+        horizontalScreenSize = 10f;
+        verticalScreenSize = 6.5f;
+        score = 0;
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        CreateSky();
+        InvokeRepeating("CreateEnemy", 1, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 
-    void CreateEnemyOne()
+    void CreateEnemy()
     {
-        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.9f, verticalScreenSize, 0), Quaternion.Euler(180, 0, 0));
     }
 
-    void CreateEnemyTwo()
+    void CreateSky()
     {
-        Instantiate(enemyTwoPrefab, new Vector3(9f, Random.Range(0, 6f), 0), Quaternion.identity);
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloudPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize), Random.Range(-verticalScreenSize, verticalScreenSize), 0), Quaternion.identity);
+        }
+        
+    }
+    public void AddScore(int earnedScore)
+    {
+        score = score + earnedScore;
     }
 
+    public void ChangeLivesText (int currentLives)
+    {
+        livesText.text = "Lives: " + currentLives;
+    }
 }
